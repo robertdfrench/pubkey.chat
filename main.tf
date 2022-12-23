f8bf66f72105b7eec39d9aa3a25fce8d04d14bf5
Robert D. French <robert@robertdfrench.me>
2022-12-23 12:00:28 -0500

    Create a separate server for Atlanta

--- main.tf
+++ main.tf
@@ -32,9 +32,26 @@ resource "vultr_dns_domain" "root" {
   domain = "pubkey.chat"
 }
 
+data "vultr_os" "openbsd" {
+  filter {
+    name   = "name"
+    values = ["OpenBSD 7.2 x64"]
+  }
+}
+
+resource "vultr_instance" "atl" {
+  plan        = "vhp-1c-1gb-intel"
+  region      = "atl"
+  os_id       = data.vultr_os.openbsd.id
+  label       = "atl"
+  tags        = ["infrastructure"]
+  hostname    = "atl.pubkey.chat"
+  enable_ipv6 = true
+}
+
 resource "vultr_dns_record" "www" {
   domain = vultr_dns_domain.root.id
   name   = "www"
-  data   = "149.28.43.177"
+  data   = vultr_instance.atl.main_ip
   type   = "A"
 }
