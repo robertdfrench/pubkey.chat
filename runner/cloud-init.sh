fc6d28a7c0fbd3314601a9ba565277cae6cf984f
Robert D. French <robert@robertdfrench.me>
2022-12-27 00:09:16 -0500

    Launch and configure new instance

--- runner/cloud-init.sh
+++ runner/cloud-init.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+sudo -u ubuntu /bin/bash - << 'EOF'
+set -ex
+cd /home/ubuntu
 mkdir actions-runner && cd actions-runner
-curl -o actions-runner-osx-x64-2.299.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-osx-x64-2.299.1.tar.gz
-echo "b0128120f2bc48e5f24df513d77d1457ae845a692f60acf3feba63b8d01a8fdc actions-runner-osx-x64-2.299.1.tar.gz" | shasum -a 256 -c
-tar xzf ./actions-runner-osx-x64-2.299.1.tar.gz
-./config.sh --url https://github.com/robertdfrench/pubkey.chat --token TOKEN
+curl -o actions-runner-linux-x64-2.299.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-2.299.1.tar.gz
+echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  actions-runner-linux-x64-2.299.1.tar.gz" > checksum
+shasum -a 256 -c checksum
+tar xzf ./actions-runner-linux-x64-2.299.1.tar.gz
+./config.sh --unattended \
+	--url https://github.com/robertdfrench/pubkey.chat \
+	--token TOKEN \
+	--replace --name runner.invalid \
+	--labels runner.invalid
+EOF
+sudo -u ubuntu /bin/bash - << 'EOF'
+set -ex
+cd /home/ubuntu/actions-runner
+nohup ./run.sh &
+EOF
