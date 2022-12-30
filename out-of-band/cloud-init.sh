bee9b36cfd17a265234ac3829dc6043f934bdf32
Robert D. French <robert@robertdfrench.me>
2022-12-30 17:54:52 -0500

    Configure runner out-of-band

--- out-of-band/cloud-init.sh
+++ out-of-band/cloud-init.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e -o pipefail
 
+# Prevent password login with root
+sudo passwd -l root
+
 # Install Hashicorp Repo
 apt-get update && apt-get install -y gnupg software-properties-common
 wget -O- https://apt.releases.hashicorp.com/gpg \
-	| gpg --dearmor \
-	| tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	| gpg --dearmor > /usr/share/keyrings/hashicorp-archive-keyring.gpg
 gpg --no-default-keyring \
 	--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
 	--fingerprint \
-	| grep "E8A0 32E0 94D8 EB4E A189 D270 DA41 8C88 A321 9F7B"
+	| grep "E8A0 32E0 94D8 EB4E A189  D270 DA41 8C88 A321 9F7B"
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
 	| tee /etc/apt/sources.list.d/hashicorp.list
 apt update
@@ -31,9 +33,7 @@ tar xzf ./actions-runner-linux-x64-2.299.1.tar.gz
 	--labels runner.invalid
 EOF
 
-# Start Actions Runner
-sudo -u ubuntu /bin/bash - << 'EOF'
-set -ex
+# Install Actions Service
 cd /home/ubuntu/actions-runner
-nohup ./run.sh &
-EOF
+./svc.sh install ubuntu
+./svc.sh start
