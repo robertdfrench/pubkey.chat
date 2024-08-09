74fb150926a7101e577f467042d9ef5247569b69
Robert D. French <robert@robertdfrench.me>
2024-08-09 11:19:59 -0400

    Move image to its own directory

--- service/image/service.py
+++ service/image/service.py
@@ -1,4 +1,5 @@
 import json
+import hashlib
 import os
 import boto3
 import time
@@ -17,6 +18,14 @@ sqs_client = boto3.client('sqs', region_name=config['DEFAULT']['region'])
 QUEUE_URL = config['DEFAULT']['queue_url']
 BUCKET_NAME = config['DEFAULT']['bucket_name']
 
+
+def compute_message_name(body: str) -> str:
+    byte_string = body.encode('utf-8')
+    digest = hashlib.sha256()
+    digest.update(byte_string)
+    return digest.hexdigest()
+    
+
 def process_messages():
     while True:
         response = sqs_client.receive_message(
@@ -29,22 +38,23 @@ def process_messages():
             continue
 
         for record in messages:
+            message_name = compute_message_name(record['body'])
             message = json.loads(record['Body'])
             profile = message['profile']
-            
+
             # Write the message to S3
             s3_client.put_object(
                 Bucket=BUCKET_NAME,
-                Key=f'{profile}.json',
+                Key=f'messages/{message_name}',
                 Body=record['Body']
             )
-            
-            # Update the topic file with the latest ID
-            #s3_client.put_object(
-            #    Bucket=BUCKET_NAME,
-            #    Key=f'{topic}',
-            #    Body=message_id
-            #)
+
+            # Update the TOPIC for profile
+            s3_client.put_object(
+                Bucket=BUCKET_NAME,
+                Key=f'topics/{profile}',
+                Body=message_name
+            )
             
             # Delete the message from SQS
             sqs_client.delete_message(
