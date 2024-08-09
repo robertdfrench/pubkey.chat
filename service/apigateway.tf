011c7185a3e69c22881e4bbbd5200c7a3e7d4ec0
Robert D. French <robert@robertdfrench.me>
2024-08-08 21:32:41 -0400

    Hook it up to sqs

--- service/apigateway.tf
+++ service/apigateway.tf
@@ -20,10 +20,21 @@ resource "aws_api_gateway_integration" "create_message" {
   rest_api_id = aws_api_gateway_rest_api.chat.id
   resource_id = aws_api_gateway_resource.messages.id
   http_method = aws_api_gateway_method.create_message.http_method
-  type        = "MOCK"
+  type        = "AWS"
+
+  credentials = aws_iam_role.api_gateway_sqs_role.arn
+
+  integration_http_method = "POST"
+  uri = "arn:aws:apigateway:${data.aws_region.current.name}:sqs:path/${aws_sqs_queue.chat_service_queue.name}"
 
   request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
+    "application/json" = <<EOF
+{
+  "Action": "SendMessage",
+  "QueueUrl": "${aws_sqs_queue.chat_service_queue.url}",
+  "MessageBody": "$input.body"
+}
+EOF
   }
 }
 
@@ -39,6 +50,10 @@ resource "aws_api_gateway_method_response" "create_message_200" {
 }
 
 resource "aws_api_gateway_integration_response" "create_message_200" {
+  depends_on = [
+    aws_api_gateway_integration.create_message
+  ]
+
   rest_api_id = aws_api_gateway_rest_api.chat.id
   resource_id = aws_api_gateway_resource.messages.id
   http_method = aws_api_gateway_method.create_message.http_method
@@ -76,6 +91,44 @@ resource "aws_api_gateway_stage" "prod" {
     format          = "$context.requestId $context.identity.sourceIp $context.identity.caller $context.identity.user $context.requestTime $context.httpMethod $context.resourcePath $context.status $context.protocol $context.responseLength"
   }
 }
+
+resource "aws_iam_role" "api_gateway_sqs_role" {
+  name = "APIGatewaySQSRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "api_gateway_sqs_policy" {
+  name        = "APIGatewaySQSPolicy"
+  description = "Policy for allowing API Gateway to send messages to SQS"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "sqs:SendMessage",
+        Resource = aws_sqs_queue.chat_service_queue.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "api_gateway_sqs_policy_attachment" {
+  role       = aws_iam_role.api_gateway_sqs_role.name
+  policy_arn = aws_iam_policy.api_gateway_sqs_policy.arn
+}
+
 
 resource "aws_iam_role" "api_gateway_cloudwatch_role" {
   name = "APIGatewayCloudWatchLogsRole"
