6c836c09b05656ccb79ce13e8bb946d82f778c98
Robert D. French <robert@robertdfrench.me>
2024-08-09 00:02:44 -0400

    Write to SQS and read from bucket

--- service/apigateway.tf
+++ service/apigateway.tf
@@ -9,11 +9,28 @@ resource "aws_api_gateway_resource" "messages" {
   path_part   = "messages"
 }
 
+resource "aws_api_gateway_resource" "messages_name" {
+  rest_api_id = aws_api_gateway_rest_api.chat.id
+  parent_id   = aws_api_gateway_resource.messages.id
+  path_part   = "{name}"
+}
+
 resource "aws_api_gateway_method" "create_message" {
   rest_api_id   = aws_api_gateway_rest_api.chat.id
   resource_id   = aws_api_gateway_resource.messages.id
   http_method   = "POST"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "get_message" {
+  rest_api_id   = aws_api_gateway_rest_api.chat.id
+  resource_id   = aws_api_gateway_resource.messages_name.id
+  http_method   = "GET"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.name" = true
+  }
 }
 
 resource "aws_api_gateway_integration" "create_message" {
@@ -27,15 +44,55 @@ resource "aws_api_gateway_integration" "create_message" {
   integration_http_method = "POST"
   uri = "arn:aws:apigateway:${data.aws_region.current.name}:sqs:path/${aws_sqs_queue.chat_service_queue.name}"
 
+  request_parameters = {
+    "integration.request.header.Content-Type" = "'application/x-www-form-urlencoded'"
+  }
+
   request_templates = {
     "application/json" = <<EOF
-{
-  "Action": "SendMessage",
-  "QueueUrl": "${aws_sqs_queue.chat_service_queue.url}",
-  "MessageBody": "$input.body"
-}
+Action=SendMessage&MessageBody=$util.urlEncode($input.body)
 EOF
   }
+}
+
+resource "aws_api_gateway_integration" "get_message" {
+  rest_api_id = aws_api_gateway_rest_api.chat.id
+  resource_id = aws_api_gateway_resource.messages_name.id
+  http_method = aws_api_gateway_method.get_message.http_method
+  credentials = aws_iam_role.api_gateway_sqs_role.arn
+
+  integration_http_method = "GET"
+  type                    = "AWS"
+  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:s3:path/${aws_s3_bucket.chat_service_bucket.id}/{name}"
+  request_parameters = {
+    "integration.request.path.name" = "method.request.path.name"
+  }
+}
+
+resource "aws_api_gateway_method_response" "get_message_200" {
+  rest_api_id = aws_api_gateway_rest_api.chat.id
+  resource_id = aws_api_gateway_resource.messages_name.id
+  http_method = aws_api_gateway_method.get_message.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "get_message_200" {
+  rest_api_id = aws_api_gateway_rest_api.chat.id
+  resource_id = aws_api_gateway_resource.messages_name.id
+  http_method = aws_api_gateway_method.get_message.http_method
+  status_code = aws_api_gateway_method_response.get_message_200.status_code
+
+  response_parameters = {
+    "method.response.header.Content-Type" = "integration.response.header.Content-Type"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.get_message
+  ]
 }
 
 resource "aws_api_gateway_method_response" "create_message_200" {
@@ -48,6 +105,7 @@ resource "aws_api_gateway_method_response" "create_message_200" {
     "application/json" = "Empty"
   }
 }
+
 
 resource "aws_api_gateway_integration_response" "create_message_200" {
   depends_on = [
@@ -66,8 +124,28 @@ resource "aws_api_gateway_integration_response" "create_message_200" {
 
 resource "aws_api_gateway_deployment" "chat" {
   depends_on = [
-    aws_api_gateway_integration.create_message
+    aws_api_gateway_integration.create_message,
   ]
+
+  triggers = {
+    # This will trigger a new deployment on changes to any of these resources
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.messages.id,
+      aws_api_gateway_method.create_message.id,
+      aws_api_gateway_integration.create_message.id,
+      aws_api_gateway_method_response.create_message_200.id,
+      aws_api_gateway_integration_response.create_message_200.id,
+      aws_api_gateway_resource.messages_name.id,
+      aws_api_gateway_method.get_message.id,
+      aws_api_gateway_integration.get_message.id,
+      aws_api_gateway_method_response.get_message_200.id,
+      aws_api_gateway_integration_response.get_message_200.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   rest_api_id = aws_api_gateway_rest_api.chat.id
 }
@@ -119,6 +197,11 @@ resource "aws_iam_policy" "api_gateway_sqs_policy" {
         Effect = "Allow",
         Action = "sqs:SendMessage",
         Resource = aws_sqs_queue.chat_service_queue.arn
+      },
+      {
+        Effect = "Allow",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.chat_service_bucket.arn}/*"
       }
     ]
   })
@@ -175,4 +258,12 @@ resource "aws_iam_role_policy_attachment" "api_gateway_cloudwatch_attachment" {
 
 resource "aws_api_gateway_account" "api_gateway_account" {
   cloudwatch_role_arn = aws_iam_role.api_gateway_cloudwatch_role.arn
+}
+
+output "write_url" {
+  value = "${aws_api_gateway_stage.prod.invoke_url}/messages"
+}
+
+output "read_url" {
+  value = "${aws_api_gateway_stage.prod.invoke_url}/messages/{name}"
 }
